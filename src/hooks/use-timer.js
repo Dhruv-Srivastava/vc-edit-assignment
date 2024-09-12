d7ea26574e21ae9835340203c78677c972f03dd1
Dhruv-Srivastava <69876724+Dhruv-Srivastava@users.noreply.github.com>
2024-09-12 23:34:20 +0530

    Removed unused useMemo

--- src/hooks/use-timer.js
+++ src/hooks/use-timer.js
@@ -1,22 +1,22 @@
-import { useEffect, useMemo, useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 export function useTimer() {
   const [isPlaying, setIsPlaying] = useState(false);
   const [time, setTime] = useState(0);
-  const startTimeRef = useRef(null); // To track when the timer started
-  const elapsedRef = useRef(0); // To track the elapsed time between updates
+  const startTimeRef = useRef(null);
+  const elapsedRef = useRef(0);
 
   useEffect(() => {
     if (isPlaying) {
-      startTimeRef.current = Date.now() - elapsedRef.current; // Adjust start time to account for elapsed time
+      startTimeRef.current = Date.now() - elapsedRef.current;
       const intervalId = setInterval(() => {
         const elapsedTime = Date.now() - startTimeRef.current;
         setTime(elapsedTime);
-      }, 10); // Update every 10ms for higher accuracy
+      }, 10);
 
       return () => {
         clearInterval(intervalId);
-        elapsedRef.current = Date.now() - startTimeRef.current; // Store elapsed time for future calculations
+        elapsedRef.current = Date.now() - startTimeRef.current;
       };
     } else {
       clearInterval(startTimeRef.current);
@@ -24,17 +24,17 @@ export function useTimer() {
   }, [isPlaying]);
 
   function restartTimer() {
-    setTime(0); // Reset time
-    setIsPlaying(false); // Stop the timer if it's running
-    elapsedRef.current = 0; // Reset elapsed time
+    setTime(0);
+    setIsPlaying(false);
+    elapsedRef.current = 0;
   }
 
   function pauseTimer() {
-    setIsPlaying(false); // Simply toggle playing, cleanup will handle interval
+    setIsPlaying(false);
   }
 
   function startTimer() {
-    setIsPlaying(true); // Start the timer
+    setIsPlaying(true);
   }
 
   return { time, isPlaying, startTimer, pauseTimer, restartTimer };
