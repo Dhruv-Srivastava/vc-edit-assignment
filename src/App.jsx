eb0387a02f6992f0596fe1cb859a5ddd14853161
Dhruv-Srivastava <69876724+Dhruv-Srivastava@users.noreply.github.com>
2024-09-13 02:13:52 +0530

    Add minor css changes

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,8 @@ import ScribblePencilLogo from "./components/PencilScribble";
 export default function App() {
   return (
     <div className="container mx-auto px-[10px] flex flex-col min-h-screen items-center gap-10 lg:gap-20">
-      <div className="mt-10 flex items-center gap-4">
-        <h1 className="text-3xl text-white">Viralcut - Transcript Editor</h1>
+      <div className="mt-10 flex flex-col items-center gap-4 md:flex-row">
+        <h1 className="text-[1.6rem] text-white">Viralcut - Transcript Editor</h1>
         <ScribblePencilLogo />
     </div>
       <TranscriptEditor initialTranscript={initialTranscript} />
